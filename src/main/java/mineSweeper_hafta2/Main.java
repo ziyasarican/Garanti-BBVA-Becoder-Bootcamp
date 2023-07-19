187ceac42f0a613ab2f746cb7acb04df8f692832
ziya <saricanziya@gmail.com>
2023-07-19 23:21:26 +0300

    commit deneme

---
+++ src/main/java/mineSweeper_hafta2/Main.java
@@ -0,0 +1,9 @@
+package mineSweeper_hafta2;
+
+public class Main {
+        public static void main(String[] args) {
+                MineSweeper mineSweeper = new MineSweeper(3,3);
+                mineSweeper.game();
+        }
+
+}
